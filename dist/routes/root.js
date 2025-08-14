91ffe9f98cc45eef232f9405017752af7a3958c4
Tayanna Amorim <105131804+TayAmorim@users.noreply.github.com>
2025-08-14 23:37:44 +0000

    :bug: refactor: removvendo a pasta dist do controle de versao

--- dist/routes/root.js
+++
@@ -1,9 +0,0 @@
-export default async function rootRoutes(fastify) {
-    fastify.get('/', async (request, reply) => {
-        return {
-            status: 'online',
-            message: 'Bem-vindo à API da Plataforma de Educação Inclusiva (Pei)!',
-            timestamp: new Date().toISOString(),
-        };
-    });
-}
