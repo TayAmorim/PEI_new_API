c47a601694b1a0c58f4b607fdbe88f4f4e2db6f1
Tayanna Amorim <105131804+TayAmorim@users.noreply.github.com>
2025-08-15 00:22:18 +0000

    fix: adicionando dist para teste

---
+++ dist/plugins/authenticate.js
@@ -0,0 +1,15 @@
+import fastifyPlugin from 'fastify-plugin';
+const authenticatePlugin = async (fastify) => {
+    fastify.addHook('onRequest', async (request, reply) => {
+        if (!request.headers.authorization) {
+            return reply.code(401).send({ error: 'Você não tem permissão para acessar' });
+        }
+        const token = request.headers.authorization.replace('Bearer ', '');
+        const { data: { user }, error } = await request.server.supabase.auth.getUser(token);
+        if (error || !user) {
+            return reply.code(401).send({ error: 'Token inválido' });
+        }
+        request.user = user;
+    });
+};
+export default fastifyPlugin(authenticatePlugin);
