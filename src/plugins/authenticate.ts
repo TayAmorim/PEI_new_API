b8c3cab9db31e1dd477d24171b4d93e8f90644fb
Tayanna Amorim <105131804+TayAmorim@users.noreply.github.com>
2025-08-23 13:20:57 +0000

    :sparkles: feat: config prettier e editorconfig

--- src/plugins/authenticate.ts
+++ src/plugins/authenticate.ts
@@ -1,28 +1,34 @@
-import { FastifyPluginAsync } from 'fastify';
-import fastifyPlugin from 'fastify-plugin';
-import { User } from '@supabase/supabase-js';
+import { FastifyPluginAsync } from "fastify";
+import fastifyPlugin from "fastify-plugin";
+import { User } from "@supabase/supabase-js";
 
-
-declare module 'fastify' {
+declare module "fastify" {
   interface FastifyRequest {
     user?: User;
   }
 }
 
-
 const authenticatePlugin: FastifyPluginAsync = async (fastify) => {
-  fastify.addHook('onRequest', async (request, reply) => {
-     if (request.url.startsWith('/auth/register') || request.url.startsWith('/auth/login')) {
-      return; 
+  fastify.addHook("onRequest", async (request, reply) => {
+    if (
+      request.url.startsWith("/auth/register") ||
+      request.url.startsWith("/auth/login")
+    ) {
+      return;
     }
 
     const token = request.cookies.acess_token;
 
     if (!token) {
-      return reply.code(401).send({ error: 'Você não tem permissão para acessar' });
+      return reply
+        .code(401)
+        .send({ error: "Você não tem permissão para acessar" });
     }
-    
-    const { data: { user }, error } = await request.server.supabase.auth.getUser(token);
+
+    const {
+      data: { user },
+      error,
+    } = await request.server.supabase.auth.getUser(token);
 
     if (error || !user) {
       return reply.send({ error: error?.message });
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
-import Fastify from 'fastify';
-import dotenv from 'dotenv';
-import cors from '@fastify/cors';
-import fastifyCookie from '@fastify/cookie';
-import supabasePlugin from './plugins/supabase.js';
-import activityRoutes from './routes/activities.js';
-import authenticatePlugin from './plugins/authenticate.js'
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-import authRoutes from './routes/auth.routes.js';
+import Fastify from "fastify";
+import dotenv from "dotenv";
+import cors from "@fastify/cors";
+import fastifyCookie from "@fastify/cookie";
+import supabasePlugin from "./plugins/supabase.js";
+import activityRoutes from "./routes/activities.js";
+import authenticatePlugin from "./plugins/authenticate.js";
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+import authRoutes from "./routes/auth.routes.js";
 
 dotenv.config();
 
@@ -18,31 +18,28 @@ await app.register(fastifyCookie, {
   secret: process.env.COOKIE_SECRET,
 });
 
+app.register(cors, {
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
 
-app.register(cors, { origin: (origin, callback) => {
-  if (!origin) return callback(null, true); 
-
-  const allowedOrigins = [
-    'http://localhost:3000'
-  ]
-  if (/\.app\.github\.dev$/.test(origin) || allowedOrigins.includes(origin)) {
+    const allowedOrigins = ["http://localhost:3000"];
+    if (/\.app\.github\.dev$/.test(origin) || allowedOrigins.includes(origin)) {
       return callback(null, true);
-  }
-  return callback(new Error('Not allowed by CORS'), false);
-}, 
+    }
+    return callback(new Error("Not allowed by CORS"), false);
+  },
   credentials: true,
-  allowedHeaders: ['Content-Type', 'Authorization'], 
-  methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
- });
- 
+  allowedHeaders: ["Content-Type", "Authorization"],
+  methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS"],
+});
+
 app.register(supabasePlugin);
-app.register(authRoutes, { prefix: "/auth" })
+app.register(authRoutes, { prefix: "/auth" });
 
-app.register(authenticatePlugin)
-app.register(activityRoutes, { prefix: '/activities' });
-
+app.register(authenticatePlugin);
+app.register(activityRoutes, { prefix: "/activities" });
 
 export default async (req: VercelRequest, res: VercelResponse) => {
   await app.ready();
-  app.server.emit('request', req, res);
+  app.server.emit("request", req, res);
 };
