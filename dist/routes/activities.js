19ea261bfe2e862eacd5923a42cf5de0449535e3
Tayanna Amorim <105131804+TayAmorim@users.noreply.github.com>
2025-08-23 14:42:33 +0000

    :sparkles: feat: nova rota post atividades

--- dist/routes/activities.js
+++ dist/routes/activities.js
@@ -1,3 +1,18 @@
+const activityBodyJsonSchema = {
+    type: "object",
+    required: ["title", "date"],
+    properties: {
+        title: { type: "string" },
+        date: { type: "string" },
+        status: { type: "string", enum: ["pendente", "concluída", "cancelada"], default: "pendente" },
+        avalition: { type: "string" },
+        objecttives: { type: "string" },
+        type: { type: "string" },
+    },
+};
+const schema = {
+    body: activityBodyJsonSchema,
+};
 export default async function activityRoutes(fastify) {
     fastify.get("/", async (request, reply) => {
         const { data, error } = await fastify.supabase
@@ -8,5 +23,21 @@ export default async function activityRoutes(fastify) {
             return reply.code(500).send({ message: "Erro ao buscar atividades." });
         }
         return data;
+    });
+    fastify.post("/", { schema }, async (request, reply) => {
+        const { title, date, status, avalition, objecttives, type, } = (await request.body);
+        if (!request.user || !request.user.id) {
+            return reply.code(401).send({ message: "Usuário não autenticado." });
+        }
+        const { data, error } = await fastify.supabase
+            .from("atividades")
+            .insert([{ title, status, date, type, avalition, objecttives, user_id: request.user.id }])
+            .select()
+            .single();
+        if (error) {
+            fastify.log.error(error);
+            return reply.code(500).send({ message: "Erro ao criar atividade." });
+        }
+        return reply.code(201).send(data);
     });
 }
--- src/routes/activities.ts
+++ src/routes/activities.ts
@@ -1,4 +1,22 @@
 import { FastifyInstance } from "fastify";
+import { ActivityProps } from "../types/index.js";
+
+const activityBodyJsonSchema = {
+  type: "object",
+  required: ["title", "date"],
+  properties: {
+    title: { type: "string" },
+    date: { type: "string" },
+    status: { type: "string", enum: ["pendente", "concluída", "cancelada"], default: "pendente" },
+    avalition: { type: "string"},
+    objecttives: { type: "string" },
+    type: { type: "string" },
+  },
+};
+
+const schema = {
+  body: activityBodyJsonSchema,
+};
 
 export default async function activityRoutes(fastify: FastifyInstance) {
   fastify.get("/", async (request, reply) => {
@@ -11,5 +29,22 @@ export default async function activityRoutes(fastify: FastifyInstance) {
       return reply.code(500).send({ message: "Erro ao buscar atividades." });
     }
     return data;
+  });
+  fastify.post("/", { schema },  async (request, reply) => {
+    const { title, date, status, avalition, objecttives, type, } = (await request.body) as ActivityProps;
+
+    if (!request.user || !request.user.id) {
+      return reply.code(401).send({ message: "Usuário não autenticado." });
+    }
+    const { data, error } = await fastify.supabase
+      .from("atividades")
+      .insert([{ title, status, date, type, avalition, objecttives, user_id: request.user.id }])
+      .select()
+      .single();  
+    if (error) {
+      fastify.log.error(error);
+      return reply.code(500).send({ message: "Erro ao criar atividade." });
+    }
+    return reply.code(201).send(data);
   });
 }
